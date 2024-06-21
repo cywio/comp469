84ad05a696337eb7837f7b3155ecbab15714d351
Chris Walsh <christianywalsh@gmail.com>
2024-06-21 10:53:15 -0700

    Add winning check to columns

--- main.py
+++ main.py
@@ -26,6 +26,25 @@ class Connect4:
         self.board[rowToInsertInto][column] = self.current_player
         self.current_player = self.player1 if self.current_player == self.player2 else self.player2
     
+    def check_if_winning(self):
+        winning_player = None
+        
+        # check diagnals
+        # @todo
+        
+        # check rows
+        #@todo
+        
+        # check columns
+        for col in range(4):
+            items = [item[col] for item in self.board]
+            plays = set(items)
+            if len(plays) == 1 and set(items) != {' '}:
+                winning_player = list(plays)[0]
+                
+        if winning_player:
+            print(f"Player {winning_player} wins!")
+            
 if __name__ == '__main__':
     game = Connect4()
     game.make_move(0) # X
@@ -40,6 +59,17 @@ if __name__ == '__main__':
     game.print_board()
     game.make_move(2) # O
     game.print_board()
+    game.check_if_winning() # should be false
+    game.make_move(1) # X
+    game.make_move(0) # O
+    game.make_move(1) # X
+    game.make_move(3) # O
+    game.make_move(1) # X
+    game.make_move(3) # O
+    game.make_move(1) # X
+    game.print_board()
+    game.check_if_winning() # should be player X because we have 4 in column 1
+    
     
     
     
