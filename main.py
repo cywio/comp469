8f6b645ffa8e204f2d349ad7baa056b861409943
Chris Walsh <christianywalsh@gmail.com>
2024-07-06 00:08:18 -0700

    Update minimax algorithm

--- main.py
+++ main.py
@@ -4,9 +4,8 @@
 # Jorge Torrez
 # Brian Rojas
 
-# import keyboard
-
-board_size = 4
+board_size = 6
+max_look_ahead = 4
 
 
 class Connect4:
@@ -37,7 +36,7 @@ class Connect4:
     def open_locations(self):
         open_cols = []
         for col in range(board_size):
-            if self.board[0][col] == ' ':
+            if self.board[0][col] == ' ':  # if the top row is empty, then it's still open
                 open_cols.append(col)
         return open_cols
 
@@ -48,85 +47,94 @@ class Connect4:
                 break
         self.current_player = self.player1 if self.current_player == self.player2 else self.player2
 
-    def is_ending_move(self):
-        return self.check_if_winning()[0] or len(self.open_locations()) == 0
+    def assign_heuristic_value(self, player):
+        score = 0
 
-    def assign_heuristic_value(self, piece):
-        # TODO diagonals
-        # assign heuristic value to location
-        value = 0
-
-        # VERTICAL
-        for c in range(board_size):
-            column_values = []
-            for r in range(board_size):
-                column_values.append(self.board[r][c])
-            for r in range(board_size - 3):
-                scoring_row = []
-                for k in range(4):
-                    scoring_row.append(column_values[r + k])
-                value += self.score_row(scoring_row, piece)
-
-        # Score Horizontal
+        # rows
         for row in range(board_size):
-            row_values = []
-            for column in range(board_size):
-                row_values.append(self.board[r][column])
-                scoring_row = []
-            for column in range(board_size - 4):
-                for k in range(4):
-                    scoring_row.append(row_values[column + k])
-                value += self.score_row(scoring_row, piece)
+            for offset in range(board_size - 3):
+                # rows
+                score += self.score_move(self.board[row]
+                                         [:][offset:offset + 4], player)
+                # columns
+                score += self.score_move([item[row]
+                                          for item in self.board][offset:offset + 4], player)
 
-        return value
+        # diagonals
+        for row in range(board_size-3):
+            for col in range(board_size-3):
+                # backwards
+                score += self.score_move([self.board[row + i][col + i]
+                                          for i in range(4)], player)
+                # forward
+                score += self.score_move([self.board[row + 3 - i][col + i]
+                                          for i in range(4)], player)
 
-    def score_row(self, row, piece):
+        return score
+
+    def score_move(self, move, player):
         # will change these scores/add more rules soon
         score = 0
-        opp_piece = self.player1 if self.current_player == self.player2 else self.player2
+        other_player = self.player1 if self.current_player == self.player2 else self.player2
 
-        if row.count(piece) == 4:
+        empty_count = move.count(' ')
+        player_count = move.count(player)
+        other_player_count = move.count(other_player)
+
+        if player_count == 4:
             score += 100
-        elif row.count(piece) == 3 and row.count(' ') == 1:
+        elif player_count == 3 and empty_count == 1:
             score += 5
-        elif row.count(piece) == 2 and row.count(' ') == 2:
+        elif player_count == 2 and empty_count == 2:
             score += 2
-
-        if row.count(opp_piece) == 3 and row.count(' ') == 1:
+        if other_player_count == 3 and empty_count == 1:
             score -= 4
         return score
 
+    # https://en.wikipedia.org/wiki/Minimax#Pseudocode
     def minimax(self, depth, maximizingPlayer):
+        # get columns where i can make a move
         open_columns = self.open_locations()
-        is_terminal = self.is_ending_move()
-        if is_terminal or depth == 0:
-            if is_terminal:
-                if self.check_if_winning()[1] == self.player1:
-                    return (None, float('inf'))  # player1 wins
-                elif self.check_if_winning()[1] == self.player2:
-                    return (None, float('-inf'))  # player2 wins
+        is_leaf_node = self.check_if_winning()[0] or len(open_columns) == 0
+        if is_leaf_node or depth == 0:
+            if is_leaf_node:
+                # if either player wins at this stage, then return highest possible
+                # score to indicate it's over
+                winning_player = self.check_if_winning()[1]
+                if winning_player == self.player1:
+                    return None, float('inf')  # player1 wins
+                elif winning_player == self.player2:
+                    return None, float('-inf')  # player2 wins
                 else:
-                    return (None, 0)  # Game is a tie
-            # Depth is zero
-            return (None, self.assign_heuristic_value(self.current_player))
+                    return None, 0  # Game is a tie
+            # If we've reached the max depth, just assign the heuristic value
+            return None, self.assign_heuristic_value(self.current_player)
 
+        # maximize the score for the current player
         if maximizingPlayer:
             value = float('-inf')
             column = open_columns[0]
             for col in open_columns:
+                # borrow board from the current state and seek ahead
                 for row in reversed(range(board_size)):
                     if self.board[row][col] == ' ':
                         self.board[row][col] = self.current_player
                         break
                 self.current_player = self.player2
-                new_score = self.minimax(depth - 1, False)[1]
+                # recursively check and decrease depth to limit recursion
+                _, new_score = self.minimax(depth - 1, False)
+                # reset the board to the original state
                 self.board[row][col] = ' '
                 self.current_player = self.player1
+                # if we get a better high score than previous best, then set new column
+                # to be the best next move
                 if new_score > value:
                     value = new_score
                     column = col
             return column, value
+        # minimize the score for the opponent
         else:
+            # same as above, just flip player and score
             value = float('inf')
             column = open_columns[0]
             for col in open_columns:
@@ -135,33 +143,13 @@ class Connect4:
                         self.board[row][col] = self.current_player
                         break
                 self.current_player = self.player1
-                new_score = self.minimax(depth - 1, True)[1]
+                _, new_score = self.minimax(depth - 1, True)
                 self.board[row][col] = ' '
                 self.current_player = self.player2
                 if new_score < value:
                     value = new_score
                     column = col
             return column, value
-
-    # Pseudocode template
-    # minimax(node, depth, maximizingPlayer) is
-    #     if depth = 0 or node is a terminal node then
-    #     return the
-    #     heuristic
-    #     value
-    #     of
-    #     node
-    #
-    #     if maximizingPlayer then
-    #     value := −∞
-    #     for each child of node do
-    #     value := max(value, minimax(child, depth − 1, FALSE))
-    #     return value
-    #     else (*minimizing player *)
-    #     value := +∞
-    #     for each child of node do
-    #     value := min(value, minimax(child, depth − 1, TRUE))
-    #     return value
 
     def check_if_winning(self):
         winning_player = None
@@ -214,64 +202,69 @@ if __name__ == '__main__':
     print("2. Players take turns to drop their pieces (X or O) into one of the columns (0-6).")
     print("3. The first player to get four of their pieces in a row (vertically, horizontally, or diagonally) wins.")
     print("4. To make a move, press the corresponding number key (0-6) on your keyboard.")
-    print("5. Press 'q' to exit the game at any time.")
+    print("5. Press CTRL+C to exit the game at any time.")
     print("-----------------------------------------")
 
-    while True:
-        game.print_board()
-
-        # if keyboard.read_key() == "q":
-        #     break
-
-        player = game.current_player
-
-        move_input = input(
-            f"Player {game.get_current_player()}, enter your move: ")
-        if not move_input.isdigit():
-            print(f"Enter a number within 0 to {board_size-1}")
-            continue
-        move = int(move_input)
-        if (move >= board_size):
-            print(f"Choose a row that is within 0 to {board_size-1}")
-            continue
-
-        game.make_move(move)
-
-        # Suggest the next move
-        suggested_move, _ = game.minimax(3,
-                                         game.current_player == game.player1)  # You can adjust the depth based on performance needs
-        print(
-            f"Suggested next move for Player {game.get_current_player()}: Column {suggested_move}")
-
-        if game.check_if_winning()[0]:
+    try:
+        while True:
             game.print_board()
-            print(f"Player {player} wins!")
 
-            if game.get_current_player() == Connect4.player1:
-                score[Connect4.player2] += 1
-            else:
-                score[Connect4.player1] += 1
+            player = game.current_player
 
+            # Suggest the next move
+            suggested_move, score = game.minimax(max_look_ahead,
+                                                 game.current_player == game.player1)  # You can adjust the depth based on performance needs
             print(
-                f"Score - Player {Connect4.player1}: {score[Connect4.player1]} | Player {Connect4.player2}: {score[Connect4.player2]}")
+                f"Suggested next move for Player {game.get_current_player()}: Column {suggested_move} (score: {score})")
 
-            play_again = input("Do you want to play again? (yes/no): ").lower()
-            if play_again == 'yes':
-                game.reset_game()
+            if game.current_player == game.player2:
+                game.make_move(suggested_move)
             else:
-                break
+                move_input = input(
+                    f"Player {game.get_current_player()}, enter your move: ")
+                if not move_input.isdigit():
+                    print(f"Enter a number within 0 to {board_size-1}")
+                    continue
+                move = int(move_input)
+                if (move >= board_size):
+                    print(f"Choose a row that is within 0 to {board_size-1}")
+                    continue
 
-        if len(game.open_locations()) == 0:
-            game.print_board()
-            print(f"Tie Game")
+                game.make_move(move)
 
-            print(
-                f"Score - Player {Connect4.player1}: {score[Connect4.player1]} | Player {Connect4.player2}: {score[Connect4.player2]}")
+            if game.check_if_winning()[0]:
+                game.print_board()
+                print(f"Player {player} wins!")
 
-            play_again = input("Do you want to play again? (yes/no): ").lower()
-            if play_again == 'yes':
-                game.reset_game()
-            else:
-                break
+                if game.get_current_player() == Connect4.player1:
+                    score[Connect4.player2] += 1
+                else:
+                    score[Connect4.player1] += 1
 
-    print("Thanks for playing!")
+                print(
+                    f"Score - Player {Connect4.player1}: {score[Connect4.player1]} | Player {Connect4.player2}: {score[Connect4.player2]}")
+
+                play_again = input(
+                    "Do you want to play again? (yes/no): ").lower()
+                if play_again == 'yes':
+                    game.reset_game()
+                else:
+                    break
+
+            if len(game.open_locations()) == 0:
+                game.print_board()
+                print(f"Tie Game")
+
+                print(
+                    f"Score - Player {Connect4.player1}: {score[Connect4.player1]} | Player {Connect4.player2}: {score[Connect4.player2]}")
+
+                play_again = input(
+                    "Do you want to play again? (yes/no): ").lower()
+                if play_again == 'yes':
+                    game.reset_game()
+                else:
+                    break
+
+        print("\n\nThanks for playing!")
+    except KeyboardInterrupt:
+        print("\n\nThanks for playing!")
